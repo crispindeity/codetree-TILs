61ebf387d9a024597d729222c69b924642076960
crispin <h.c.shin.dev09@gmail.com>
2024-05-21 13:14:32 +0900

    Title: 숫자로 이루어진 사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240521/숫자로 이루어진 사각형/rectangle-with-a-number.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val input = br.readLine().toInt()
+    var count = 
+    for (i in 1..input) {
+        for (j in 1..input) {
+            if (count == 10) {
+                count = 1
+            }
+            print("$count ")
+            count++
+        }
+        println()
+    }
+}
