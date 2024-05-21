f38709c5dba441d4865fbd637b339bbbf0e9eb66
crispin <h.c.shin.dev09@gmail.com>
2024-05-21 12:44:01 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 228ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240521/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val input = readLine()
+    val splitedInput = input!!.split(" ")
+    
+    for (i in 1..splitedInput[0].toInt()) {
+        for (j in 1..splitedInput[1].toInt()) {
+            print(1)
+        }
+        println()
+    }
+}
